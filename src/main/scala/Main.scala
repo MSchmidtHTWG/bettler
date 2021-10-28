d3284145132874cef47b0a6a16211f7700ebbad8
lukasdoetterl <lukasdoetterl@hotmail.com>
2021-10-28 17:58:19 +0200

    lal

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,22 @@
 @main def Main: Unit =
   
-  println("Welcome to Bettler")
-  println(mesh())
 
+
+  println("Welcome to Bettler")
+  //println(mesh())
+
+  for (i <- 1 to playerNumber){
+    println("Spieler "+ i + " :")
+    println(mesh(4,cardNumber))
+  }
+  println(mesh(fieldSize, fieldSize, fieldSize))
+
+
+  
+//val i = 0
+val cardNumber = 7
+val playerNumber = 5
+val fieldSize = 2
 val eol = sys.props("line.separator")  
 
 def cell(cellwidth: Int = 3, cellNum: Int =3) = 
@@ -12,8 +26,8 @@ def bar(cellwidth: Int = 3, cellNum: Int =3) =
   ("+" + "-" * cellwidth) * cellNum + "+" + eol
 
 
-def mesh(cellwidth: Int = 3, cellNum: Int = 3) =
-  (bar(cellwidth, cellNum) + cell(cellwidth,cellNum)) * cellNum + bar(cellwidth, cellNum)
+def mesh(cellwidth: Int = 3, cellNum: Int = 3, rows: Int = 1) =
+  (bar(cellwidth, cellNum) + cell(cellwidth,cellNum)) * rows + bar(cellwidth, cellNum)
 
 
 
