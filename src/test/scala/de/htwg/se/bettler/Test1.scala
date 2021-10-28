49e4627dc1e79be0dbe9927b72b59964b81829dc
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-10-28 19:16:01 +0200

    commit

--- src/test/scala/de/htwg/se/bettler/Test1.scala
+++ src/test/scala/de/htwg/se/bettler/Test1.scala
@@ -8,6 +8,25 @@ class Test1 extends AnyWordSpec {
    "have a bar as String of Form '+----+----+----+----+----+----+----+'" in {
     bar() should be ("+----+----+----+----+----+----+----+" + eol)
    }
- 
+   "have a scalable bar" in {
+    bar(1,1) should be ("+-+" + eol)
+    bar(2,1) should be ("+--+" + eol)
+    bar(2,2) should be ("+--+--+" + eol)
+   }
+   "have a cell as String of Form '|   |   |   |" in {
+    cell() should be ("|   |   |   |" + eol)
+   }
+   "have a scalable cell" in {
+    cell(1,1) should be ("| |" + eol)
+    cell(2,1) should be ("|  |" + eol)
+    cell(2,2) should be ("|  |  |" + eol)
+   }
+   "have a player as String of Form 'Spieler 1 : + eol + +----+----+----+----+----+----+----+ + eol + |    |    |    |    |    |    |    | + eol + +----+----+----+----+----+----+----+" in {
+     player() should be ("Spieler 1 :" + eol + "+----+----+----+----+----+----+----+" + eol + "|    |    |    |    |    |    |    |" + eol + "+----+----+----+----+----+----+----+" + eol)
+   }
+   "have a number of players with varying cards" in {
+     player(1,1) should be ("Spieler 1 :" + eol + "+----+" + eol + "|    |" + eol + "+----+" + eol)
+     player(2,2) should be ("Spieler 2 :" + eol + "+----+----+" + eol + "|    |    |" + eol + "+----+----+" + eol)
+   }
  } 
 }
