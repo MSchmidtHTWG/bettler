679f9e0aeb5b42b3cfb57a65c2daba5cc9ce6903
lukasdoetterl <lukasdoetterl@hotmail.com>
2021-10-28 17:01:54 +0200

    print

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, worlddä#mfgöwk!")
+
+
+  val eol = sys.props("line.separator")  
+
+  def bar(cellwidth: Int = 3, cellNum: Int =3) = 
+    ("|" + " " * cellwidth) * cellNum + "|" + eol
+
+  def cell(cellwidth: Int = 3, cellNum: Int =3) =
+    ("+" + "-" * cellwidth) * cellNum + "+" + eol
+
+
+  def mesh(cellwidth: Int = 3, cellNum: Int = 3) =
+    (bar(cellwidth, cellNum) + cell(cellwidth,cellNum))* cellNum + bar(cellwidth, cellNum)
+
+    println("Welcome to Bettler")
+    println(mesh())
+
+
+
+ 
+
   }
 }
