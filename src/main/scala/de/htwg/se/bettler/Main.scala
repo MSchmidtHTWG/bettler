dec2f2a1b492bd39ae9d40b60f0c249e12c096cf
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-04 19:59:21 +0100

    a

--- src/main/scala/de/htwg/se/bettler/Main.scala
+++ src/main/scala/de/htwg/se/bettler/Main.scala
@@ -4,11 +4,13 @@ import scala.util.Random
 
 @main def Main: Unit =
   val deck = new deck
-  deck.cards = Random.shuffle(deck.cards)
+  deck.shuffle();
+  var spieler1 = deck.draw();
+  var spieler2 = deck.draw();
   val field = new field
   field.printField()
-
-
+  println(spieler1)
+  println(spieler2)
 
 
 
