3546e650b37d5d3c8de8ba706137506c0bb125f5
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-04 19:05:44 +0100

    deck

--- src/main/scala/de/htwg/se/bettler/Main.scala
+++ src/main/scala/de/htwg/se/bettler/Main.scala
@@ -1,11 +1,13 @@
 package scala
 
+import scala.util.Random
+
 @main def Main: Unit =
-  
+  val deck = new deck
+  deck.cards = Random.shuffle(deck.cards)
   val field = new field
   field.printField()
-  val karte = card('K','9')
-  print(karte.symbol)
+
 
 
 
