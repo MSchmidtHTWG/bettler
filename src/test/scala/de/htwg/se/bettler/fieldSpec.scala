4a2d51421b9117f28550e8a9acbca1cb6a3069c8
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-05 09:04:44 +0100

    field

--- src/test/scala/de/htwg/se/bettler/fieldSpec.scala
+++ src/test/scala/de/htwg/se/bettler/fieldSpec.scala
@@ -4,32 +4,30 @@ import org.scalatest.matchers.should.Matchers._
 import org.scalactic.Prettifier.default
 class fieldSpec extends AnyWordSpec {
 
-       var player1 = ('H', "7")::('S', "ACE") :: Nil;
-       var player2 = ('H', "9")::('C', "J") :: Nil;
-       var board1 = ('D', "10")::('C', "7") :: Nil;
-       var card = ('H', "7")
+  var player1 = ('H', "7")::('S', "ACE") :: Nil;
+  var player2 = ('H', "9")::('C', "J") :: Nil;
+  var board1 = ('D', "10")::('C', "7") :: Nil;
+  var card = ('H', "7");
 
-    "field " should{
+"field " should {
 
-   "have a bar as String of Form '--------------------------------------------------'" in {
+  "have a bar as String of Form '--------------------------------------------------'" in {
     field.bar() should be ("--------------------------------------------------" + field.eol)
-   }
-   "have a scalable bar" in {
+  }
+  "have a scalable bar" in {
     field.bar(100) should be ("----------------------------------------------------------------------------------------------------" + field.eol)
     field.bar(1) should be ("-" + field.eol)
     field.bar(15) should be ("---------------" + field.eol)
-   }
-   "have printeble cards '[H,7]]'" in {
-    field.printCard(card) should be ("[H,7]")
-   }
-   
-   "have a printable Board" in {
-     field.printField(board1,player1,player2) should be(field.bar() + "Spieler 1" + field.eol + "[H,7][S,ACE]" +field.eol + field.bar()+"Spieler 2" + field.eol + "[H,9][C,J]" + field.eol+field.bar()+ "[D,10][C,7]"+field.eol + field.bar())
-   }
-
-
-    }
   }
+  "have printeble cards '[H,7]]'" in {
+    field.printCard(card) should be ("[H,7]")
+  }
+    
+  "have a printable Board" in {
+    field.printField(board1,player1,player2) should be(field.bar() + "Spieler 1" + field.eol + "[H,7][S,ACE]" +field.eol + field.bar()+"Spieler 2" + field.eol + "[H,9][C,J]" + field.eol+field.bar()+ "[D,10][C,7]"+field.eol + field.bar())
+  }
+}
+}
 
 
 
