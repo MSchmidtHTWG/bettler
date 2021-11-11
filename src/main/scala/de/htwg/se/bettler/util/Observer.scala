815a7b9ecbca3664f387ef31405f5fc190981bf1
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-11 19:56:48 +0100

    Observer/Observable

--- src/main/scala/de/htwg/se/bettler/util/Observer.scala
+++ src/main/scala/de/htwg/se/bettler/util/Observer.scala
@@ -1,2 +1,11 @@
-package de.htwg.se.bettler.util
+package de.htwg.se.bettler
+package util
 
+trait Observer:
+    def update: Unit
+
+trait Observable:
+    var subscribers: Vector[Observer] = Vector()
+    def add(s: Observer) = subscribers = subscribers :+ s
+    def remove(s: Observer) = subscribers = subscribers.filterNot(o => o == s)
+    def notify = subscribers.foreach(o => o.update)
