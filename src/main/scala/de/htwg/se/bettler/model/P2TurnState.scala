12c9a7fe9bec03bcdac9275a2321867bb8826f12
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-12 12:40:19 +0100

    asd

--- src/main/scala/de/htwg/se/bettler/model/P2TurnState.scala
+++ src/main/scala/de/htwg/se/bettler/model/P2TurnState.scala
@@ -7,19 +7,19 @@ case class P2TurnState() extends State:
             val msg = "Falsche Eingabe. Spiele Karten mit 'play Karte1 Karte2 ..'. Spieler 1 ist an der Reihe."
             return Game(game.state, game.spieler1, game.spieler2, game.spielfeld, game.deck, msg)
         }
-        if game.spieler2.contains(cards) then {
-            val msg = "Spieler 1 hat diese Karten nicht."
+        if !game.spieler2.contains(cards) then {
+            val msg = "Spieler 2 hat diese Karten nicht."
             return Game(game.state, game.spieler1, game.spieler2, game.spielfeld, game.deck, msg)
         }
-        if Cards(cards).isPlayable then {
+        if !Cards(cards).isPlayable then {
             val msg = "Die Karten haben nicht den gleichen Wert."
             return Game(game.state, game.spieler1, game.spieler2, game.spielfeld, game.deck, msg)
         }
-        if game.spielfeld.isWorse(cards) then {
+        if !game.spielfeld.isWorse(cards) then {
             val msg = "Es müssen genau gleichviele Karten gespielt wie auf dem Spielfeld liegen und ihr Wert muss größer sein."
             return Game(game.state, game.spieler1, game.spieler2, game.spielfeld, game.deck, msg)
         }
         val s1 = game.spieler2.returnSet -- cards
         val board = cards
-        val state = P2TurnState()
+        val state = P1TurnState()
         return Game(state, game.spieler1, Cards(s1), Cards(board), game.deck, "Spieler 1 ist dran.")
