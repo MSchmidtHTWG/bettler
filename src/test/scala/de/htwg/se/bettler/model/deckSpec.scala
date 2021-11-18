c97951e037ec8338b853e634afc72ed79f3f15ee
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-18 18:53:33 +0100

    deckSpec

--- src/test/scala/de/htwg/se/bettler/model/deckSpec.scala
+++ src/test/scala/de/htwg/se/bettler/model/deckSpec.scala
@@ -1,26 +1,18 @@
 package de.htwg.se.bettler
 package model
 
-import Card._
-import Value._
-import Symbol._
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 import org.scalactic.Prettifier.default
 class decSpec extends AnyWordSpec {
-
-"Deck" should {
-   var deck = Deck(32)
-   var deck2 = Deck()
-   "Have a Method for drawning from a deck" in {
-
-      deck.draw().size should be (7)
-      
+   "Deck" should {
+      var deck = Deck(32)
+      var deck2 = Deck(32)
+      "Have a Method for drawning from a deck" in {
+         deck.draw().size should be (7)
+      }
+      "should be subset" in {
+      deck.draw() subsetOf deck2.deck should be (true)
+      }
    }
-   "should be subset" in {
-   deck.draw() subsetOf deck2.deck should be (true)
-   }
-}
-
-
 }
