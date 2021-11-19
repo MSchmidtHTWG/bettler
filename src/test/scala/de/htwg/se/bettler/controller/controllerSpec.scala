99183da3180d023ff22628ad2ec28008db17546d
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-11-19 09:17:32 +0100

    controllerSpec

--- src/test/scala/de/htwg/se/bettler/controller/controllerSpec.scala
+++ src/test/scala/de/htwg/se/bettler/controller/controllerSpec.scala
@@ -10,7 +10,21 @@ class controllerSpec extends AnyWordSpec {
         "have a method play, that returns a Game" in {
             val game = Game()
             val controller = Controller(game)
-            controller.game.deck.equals(game.deck) shouldBe(true)
+            val game2 = controller.play(Set.empty[Card])
+            game2.deck.equals(game.deck) shouldBe(true)
+            game2.msg == "Falsche Eingabe. Spiele Karten mit 'play Karte1 Karte2 ..'. Spieler 1 ist an der Reihe." shouldBe(true)
+        }
+        "have a method skip to skip a turn" in {
+            val game = Game()
+            val controller = Controller(game)
+            val game2 = controller.skip()
+            game2.state.isInstanceOf[P2TurnState] shouldBe(true)
+        }
+        "have a method start to start a new game" in {
+            val game = Game()
+            val controller = Controller(game)
+            val game2 = controller.start()
+            game.equals(game2) shouldBe(true)
         }
     }
 }
