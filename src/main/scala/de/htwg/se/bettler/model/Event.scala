81e3a10146b56198d50b40c2a41b399961719c06
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-12-02 20:30:00 +0100

    removed unecessary events

--- src/main/scala/de/htwg/se/bettler/model/Event.scala
+++ src/main/scala/de/htwg/se/bettler/model/Event.scala
@@ -4,7 +4,4 @@ package model
 enum Event(name : String):
     override def toString = name
     case Start extends Event("start")
-    case End extends Event("end")
-    case Play extends Event("play")
     case Skip extends Event("skip")
-    case Exit extends Event("exit")
