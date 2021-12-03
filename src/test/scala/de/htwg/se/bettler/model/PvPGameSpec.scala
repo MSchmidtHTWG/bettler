bf6b8b1c646186d7a576009aaf69ce8cb37dc1bf
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-12-03 10:48:49 +0100

    PvEGameSpec and PvPGameSpec test apply

--- src/test/scala/de/htwg/se/bettler/model/PvPGameSpec.scala
+++ src/test/scala/de/htwg/se/bettler/model/PvPGameSpec.scala
@@ -5,10 +5,10 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 import de.htwg.se.bettler.util.GameMemento
 
-class PvEGameSpec extends AnyWordSpec {
-    "A PvEGame" should {
+class PvPGameSpec extends AnyWordSpec {
+    "A PvPGame" should {
         "have a factory method that creates a random new PvEGame with 2 players" in {
-            val game = PvEGame()
+            val game = PvPGame()
             game.getBoard().size shouldBe(0)
             game.getPlayers().foreach(x => x.size shouldBe(7))
         }
