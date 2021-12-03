761a86dbd0b99a972a1ef7f05cb4469195732cde
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-12-03 10:51:17 +0100

    test .apply()

--- src/test/scala/de/htwg/se/bettler/model/PvEGameSpec.scala
+++ src/test/scala/de/htwg/se/bettler/model/PvEGameSpec.scala
@@ -8,7 +8,7 @@ import de.htwg.se.bettler.util.GameMemento
 class PvEGameSpec extends AnyWordSpec {
     "A PvEGame" should {
         "have a factory method that creates a random new PvEGame with 2 players" in {
-            val game = PvEGame()
+            val game = PvEGame.apply()
             game.getBoard().size shouldBe(0)
             game.getPlayers().foreach(x => x.size shouldBe(7))
         }
--- src/test/scala/de/htwg/se/bettler/model/PvPGameSpec.scala
+++ src/test/scala/de/htwg/se/bettler/model/PvPGameSpec.scala
@@ -8,7 +8,7 @@ import de.htwg.se.bettler.util.GameMemento
 class PvPGameSpec extends AnyWordSpec {
     "A PvPGame" should {
         "have a factory method that creates a random new PvEGame with 2 players" in {
-            val game = PvPGame()
+            val game = PvPGame.apply()
             game.getBoard().size shouldBe(0)
             game.getPlayers().foreach(x => x.size shouldBe(7))
         }
