eb9224b39950f3802eb716a2d44bc532f5b8e550
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-12-03 10:43:22 +0100

    UndoManager test undo + redo

--- src/test/scala/de/htwg/se/bettler/controller/UndoManagerSpec.scala
+++ src/test/scala/de/htwg/se/bettler/controller/UndoManagerSpec.scala
@@ -8,14 +8,29 @@ import model._
 class UndoManagerSpec extends AnyWordSpec {
     "An UndoManager" should {
         "have a method to undo a command" in {
+            GameStateContext.setState(StartState())
             val undomanager = UndoManager()
             val game = Game("pvp")
-            undomanager.doStep(PlayCommand(Controller(game)))
             val controller = Controller(game)
+            undomanager.doStep(PlayCommand(controller))
             GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(0)
             controller.doAndNotify(controller.skip)
             GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(1)
-            controller.undo
+            undomanager.undoStep
+            GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(0)
+        }
+        "have a method to redo a command" in {
+            GameStateContext.setState(StartState())
+            val undomanager = UndoManager()
+            val game = Game("pvp")
+            val controller = Controller(game)
+            undomanager.doStep(PlayCommand(controller))
+            GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(0)
+            controller.doAndNotify(controller.skip)
+            GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(1)
+            undomanager.undoStep
+            GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(0)
+            undomanager.redoStep
             GameStateContext.state.asInstanceOf[PlayerTurnState].currentPlayer shouldBe(1)
         }
     }
