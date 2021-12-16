8c89123e699dacd36f49cfec66325d75184a5068
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2021-12-16 20:23:21 +0100

    conroller + field interface

--- src/main/scala/de/htwg/se/bettler/model/fieldComponent/FieldInterface.scala
+++ src/main/scala/de/htwg/se/bettler/model/fieldComponent/FieldInterface.scala
@@ -1,2 +1,12 @@
-package de.htwg.se.bettler.model.fieldComponent
+package de.htwg.se.bettler
+package model
+package fieldBaseImpl
 
+import gameComponent._
+import cardComponent.cardBaseImpl.Cards
+import cardComponent.cardBaseImpl.Card
+
+trait FieldInterface:
+    def printCard(card : Card) : String
+    def printField() : String
+    def bar(cellwidth: Int) : String
